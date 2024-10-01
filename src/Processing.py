2b1d660c6d5929f6e3377d1d9e8ba4043a9317fc
Daria <strelcova.dasha87.gmail.com>
2024-10-01 20:36:56 +0400

    minor improvements

--- src/Processing.py
+++ src/Processing.py
@@ -1,10 +1,10 @@
 from mypy.server.objgraph import Iterable
 
-dictionary_list =[
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+dictionary_list = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
 
@@ -12,14 +12,16 @@ def filter_by_state(dictionary_list: dict) -> list:
     """Функция, фильтрующая список словарей по статусу (state)"""
     new_dictionary_list = []
     for element in dictionary_list:
-        if element['state'] == 'EXECUTED':
+        if element["state"] == "EXECUTED":
             new_dictionary_list.append(element)
         else:
             continue
     return new_dictionary_list
 
 
-def sort_by_date (dictionary_list: Iterable[dict], parameter: bool = True) -> list:
+def sort_by_date(dictionary_list: Iterable[dict], parameter: bool = True) -> list:
     """Функция, сортирующая список по дате"""
-    sorted_dictionary_list = sorted(dictionary_list, key = lambda x: x['date'], reverse = parameter)
+    sorted_dictionary_list = sorted(
+        dictionary_list, key=lambda x: x["date"], reverse=parameter
+    )
     return sorted_dictionary_list
