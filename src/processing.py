a6415fe6afd7349254c3f63023b1a26cb77e673a
Daria <strelcova.dasha87.gmail.com>
2024-10-05 20:30:42 +0400

    new branch with improved code and tests

--- src/processing.py
+++ src/processing.py
@@ -1,22 +1,17 @@
 from mypy.server.objgraph import Iterable
 
 
-dictionary_list = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-
 def filter_by_state(dictionary_list: dict) -> list:
     """Функция, фильтрующая список словарей по статусу (state)"""
     new_dictionary_list = []
-    for element in dictionary_list:
-        if element["state"] == "EXECUTED":
-            new_dictionary_list.append(element)
-        else:
-            continue
+    if dictionary_list == []:
+        raise ValueError("Нет данных. Ошибка ввода.")
+    else:
+        for element in dictionary_list:
+            if element["state"] == "EXECUTED":
+                new_dictionary_list.append(element)
+            else:
+                continue
     return new_dictionary_list
 
 
--- src/widget.py
+++ src/widget.py
@@ -3,28 +3,25 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(account_information: str) -> str:
     """Функция, маскирует номер карты/счета"""
-    card_account_type = ""
     card_account_number = ""
+    space_symbol_index = account_information.rfind(" ")
 
     for symbol in account_information:
-        if symbol.isalpha():
-            card_account_type += symbol
-        elif symbol.isdigit():
+        if symbol.isdigit():
             card_account_number += symbol
-    if len(card_account_number) > 16:
+        else:
+            card_account_type = account_information[0:space_symbol_index]
+    if len(card_account_number) == 20:
         return f"{card_account_type} {get_mask_account(card_account_number)}"
-    elif len(card_account_number) == 16:
+    elif len(card_account_number) == 16 or len(card_account_number) == 18 or len(card_account_number) == 19:
         return f"{card_account_type} {get_mask_card_number(card_account_number)}"
     else:
-        return f"Ошибка ввода"
-
-
-print(mask_account_card("Maestro 1596837868705199"))
+        raise ValueError("Ошибка ввода!")
 
 
 def get_date(information: str) -> str:
     """Функция выводит корректную дату"""
-    return f"{information[5:7]}.{information[8:10]}.{information[0:4]}"
-
-
-print(get_date("2024-03-11T02:26:18.671407"))
+    if information == "":
+        return "No date"
+    else:
+        return f"{information[5:7]}.{information[8:10]}.{information[0:4]}"
