5e99eb9f804c7ac41b150510ae3f921a2c2cdc97
Daria <strelcova.dasha87.gmail.com>
2024-10-06 21:06:30 +0400

    new branch with improved code and tests

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from mypy.server.objgraph import Iterable
 
 
-def filter_by_state(dictionary_list: dict) -> list:
+def filter_by_state(dictionary_list: list) -> list:
     """Функция, фильтрующая список словарей по статусу (state)"""
     new_dictionary_list = []
     if dictionary_list == []:
@@ -17,7 +17,5 @@ def filter_by_state(dictionary_list: dict) -> list:
 
 def sort_by_date(dictionary_list: Iterable[dict], parameter: bool = True) -> list:
     """Функция, сортирующая список словарей по дате (date)"""
-    sorted_dictionary_list = sorted(
-        dictionary_list, key=lambda x: x["date"], reverse=parameter
-    )
+    sorted_dictionary_list = sorted(dictionary_list, key=lambda x: x["date"], reverse=parameter)
     return sorted_dictionary_list
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,19 +1,23 @@
 import pytest
+
 from src.masks import get_mask_account, get_mask_card_number
 
-@pytest.mark.parametrize("card_numbers, expected_result", [("7000792289606361", "7000 79** **** 6361"),
-                                                           (2345896599990000, "2345 89** **** 0000")]
-                         )
-def test_get_mask_card_number(card_numbers, expected_result):
+
+@pytest.mark.parametrize(
+    "card_numbers, expected_result",
+    [("7000792289606361", "7000 79** **** 6361"), (2345896599990000, "2345 89** **** 0000")],
+)
+def test_get_mask_card_number(card_numbers: str, expected_result: str) -> None:
     assert get_mask_card_number(card_numbers) == expected_result
 
+
 @pytest.fixture
-def test_account_numbers():
+def test_account_numbers() -> None:
     assert get_mask_account(23458965999900003333) == "**3333"
 
 
-@pytest.mark.parametrize("acc_numbers, expected_result", [("73654108430135874305", "**4305"),
-                                                          (73654108430135874305, "**4305")]
-                         )
-def test_get_mask_account(acc_numbers, expected_result):
+@pytest.mark.parametrize(
+    "acc_numbers, expected_result", [("73654108430135874305", "**4305"), (73654108430135874305, "**4305")]
+)
+def test_get_mask_account(acc_numbers: str, expected_result: str) -> None:
     assert get_mask_account(acc_numbers) == expected_result
