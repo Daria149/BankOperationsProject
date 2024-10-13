2579bc6f8d9c3a5fcae43271f93f95e27800c49d
Daria149 <strelcova.dasha87@gmail.com>
2024-10-13 10:35:38 +0400

    retoque

--- src/processing.py
+++ src/processing.py
@@ -1,25 +1,5 @@
 from typing import List
 
-
-def filter_by_state(dictionary_list: list) -> list:
-    """Функция, фильтрующая список словарей по статусу (state)"""
-    new_dictionary_list = []
-    if dictionary_list == []:
-        raise ValueError("Нет данных. Ошибка ввода.")
-    else:
-        for element in dictionary_list:
-            if element["state"] == "EXECUTED":
-                new_dictionary_list.append(element)
-            else:
-                continue
-    return new_dictionary_list
-
-
-def sort_by_date(dictionary_list: Iterable[dict], parameter: bool = True) -> list:
-    """Функция, сортирующая список словарей по дате (date)"""
-    sorted_dictionary_list = sorted(dictionary_list, key=lambda x: x["date"], reverse=parameter)
-    return sorted_dictionary_list
-
 dictionary_list = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -29,7 +9,7 @@ dictionary_list = [
 
 
 def filter_by_state(dictionary_list: List[dict], state: str = "EXECUTED") -> List[dict]:
-    '''Функция фильтрует список словарей по статусу (state)'''
+    """Функция фильтрует список словарей по статусу (state)"""
     new_dictionary_list = []
     for element in dictionary_list:
         if element["state"] == state:
@@ -40,5 +20,5 @@ def filter_by_state(dictionary_list: List[dict], state: str = "EXECUTED") -> Lis
 
 
 def sort_by_date(dictionary_list: List[dict], parameter: bool = True) -> List[dict]:
-    '''Функция сортирует список словарей по дате (date)'''
+    """Функция сортирует список словарей по дате (date)"""
     return sorted(dictionary_list, key=lambda x: x["date"], reverse=parameter)
